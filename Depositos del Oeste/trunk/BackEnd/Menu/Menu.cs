5110fa5ccff4125cbbc59cbcc14619a8425b5b65
nacho692@gmail.com <nacho692@gmail.com@b374577a-d9eb-3a0c-b418-3904471a3b07>
2012-09-24 08:55:16 +0000

    Nhibernate configurado, los metodos de las clases tienen que ser virtual, por el lazy load de hibernate, asi los puede sobreescribir, mucha gente se queja de esto igualmente, pero no vi mejor manera.
    Otra cosa, la configuracion de mapeo se puede hacer con una clase de mapeo no xml, pero no averigue.
    
    Saludines

--- Depositos del Oeste/trunk/BackEnd/Menu/Menu.cs
+++ Depositos del Oeste/trunk/BackEnd/Menu/Menu.cs
@@ -6,35 +6,34 @@ using System.Threading.Tasks;
 
 namespace BackEnd
 {
-    public class Menu
+    public class Menu:Base
     {
         private int id;
-        public int Id
+        public virtual int Id
         {
             get { return id; }
             set { id = value; }
         }
 
         private int idPadre;
-        public int IdPadre
+        public virtual int IdPadre
         {
             get { return idPadre; }
             set { idPadre = value; }
         }
 
         private string nombre;
-        public string Nombre
+        public virtual string Nombre
         {
             get { return nombre; }
             set { nombre = value; }
         }
 
         private string link;
-        public string Link
+        public virtual string Link
         {
             get { return link; }
             set { link = value; }
         }
-
     }
 }
