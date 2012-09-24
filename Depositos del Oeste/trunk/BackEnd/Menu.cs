4bf3530236d9ea8dfc18788a17dc536c9f587a60
nacho692@gmail.com <nacho692@gmail.com@b374577a-d9eb-3a0c-b418-3904471a3b07>
2012-09-24 05:45:19 +0000

    Backend y nhibernate como referencia en backend

---
+++ Depositos del Oeste/trunk/BackEnd/Menu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd
+{
+    public class Menu
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private int idPadre;
+        public int IdPadre
+        {
+            get { return idPadre; }
+            set { idPadre = value; }
+        }
+
+        private string nombre;
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        private string link;
+        public string Link
+        {
+            get { return link; }
+            set { link = value; }
+        }
+
+    }
+}
