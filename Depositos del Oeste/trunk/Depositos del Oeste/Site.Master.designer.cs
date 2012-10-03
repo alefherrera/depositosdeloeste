e83ab0096b50ba6648dac97e2be115a63952d8d1
nacho692@gmail.com <nacho692@gmail.com@b374577a-d9eb-3a0c-b418-3904471a3b07>
2012-10-03 10:57:58 +0000

    Ahi hice logueo con cookies y sesion, no hice el deslogueo, igualmente hay que cambiar cosas, falta una capa de servicios en los proyectos.

--- Depositos del Oeste/trunk/Depositos del Oeste/Site.Master.designer.cs
+++ Depositos del Oeste/trunk/Depositos del Oeste/Site.Master.designer.cs
@@ -76,6 +76,15 @@ namespace Depositos_del_Oeste {
         protected global::System.Web.UI.WebControls.Label nombre;
         
         /// <summary>
+        /// desloguear control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor desloguear;
+        
+        /// <summary>
         /// cssmenu control.
         /// </summary>
         /// <remarks>
