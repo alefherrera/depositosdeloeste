d0732a6f0b31e4343aeba66fe7ead23c2d3683ab
nacho692@gmail.com <nacho692@gmail.com@b374577a-d9eb-3a0c-b418-3904471a3b07>
2012-10-03 08:29:37 +0000

    Trabajando el tema del logueo

---
+++ Depositos del Oeste/trunk/BackEnd/Validaciones.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd
+{
+    public class Validaciones
+    {
+        public static bool isNumeric(string test)
+        {
+            try
+            {
+                int.Parse(test);
+                return true;
+            }
+            catch(Exception){
+                return false;
+            }
+        }
+    }
+}
