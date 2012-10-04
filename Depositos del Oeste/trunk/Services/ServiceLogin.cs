32bcd8659be4e2ebef4d41a73019d9ec57b6b73e
nacho692@gmail.com <nacho692@gmail.com@b374577a-d9eb-3a0c-b418-3904471a3b07>
2012-10-04 03:09:06 +0000

    Agregado servicios, corregido un par de negradas y vamos a empezar a hacer excepciones personalizadas.
    TODO: Desloguear

---
+++ Depositos del Oeste/trunk/Services/ServiceLogin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BackEnd;
+
+namespace Services
+{
+    public class ServiceLogin
+    {
+        public static Usuario loguear(string legajo, string dni)
+        {
+            Usuario user = new Usuario();
+            if (!Validaciones.isNumeric(legajo))
+            {
+                throw new LoginException("Numero de Legajo incorrecto");
+            };
+
+            user.Legajo = int.Parse(legajo);
+            user.Dni = dni;
+
+            List<Usuario> usuarios = user.Select();
+            if (usuarios.Count != 1)
+            {
+                throw new LoginException("Error, el numero de legajo o la contraseña es incorrecto/a");
+            }
+
+            user.Load();
+
+            return user;
+        }
+    }
+}
---
+++ Depositos del Oeste/trunk/Services/ServiceMenu.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BackEnd;
+
+namespace Services
+{
+    public class ServiceMenu
+    {
+        //TODO: Implementar el tema de los privilegios para el usuario
+        public static string generarMenu(Usuario user){
+            //Traigo todos los menues
+            BackEnd.Menu menu = new BackEnd.Menu();
+            List<BackEnd.Menu> menuList = menu.Select();
+
+            StringBuilder stringMenu = new StringBuilder();
+            new List<BackEnd.Menu>();
+
+            //Extraigo los padres
+            List<BackEnd.Menu> menuPrincipal = new List<BackEnd.Menu>();
+            menuPrincipal = menuList.FindAll(
+                delegate(BackEnd.Menu mn)
+                {
+                    return mn.IdPadre == 0;
+                }
+            );
+
+            //Empieza aca, se puede hacer recursivo pero son solo dos niveles estaticos, no hace falta
+            stringMenu.Append("<ul>");
+            for (int i = 0; menuPrincipal.Count > i; i++)
+            {
+                //Agarro todos los que son hijos del padre que voy iterando
+                List<BackEnd.Menu> menuSecundario = new List<BackEnd.Menu>();
+                menuSecundario = new List<BackEnd.Menu>();
+                menuSecundario = menuList.FindAll(
+                    delegate(BackEnd.Menu mn)
+                    {
+                        return mn.IdPadre == menuPrincipal[i].Id;
+                    }
+                );
+
+
+                stringMenu.Append("<li class='has-sub '><a href='");
+                stringMenu.Append(menuPrincipal[i].Link);
+                stringMenu.Append("'><span>");
+                stringMenu.Append(menuPrincipal[i].Nombre);
+                stringMenu.Append("</span></a>");
+
+                if (menuSecundario.Count > 0)
+                {
+                    stringMenu.Append("<ul>");
+                    for (int j = 0; menuSecundario.Count > j; j++)
+                    {
+                        stringMenu.Append("<li class='has-sub'><a href='");
+                        stringMenu.Append(menuSecundario[j].Link);
+                        stringMenu.Append("'><span>");
+                        stringMenu.Append(menuSecundario[j].Nombre);
+                        stringMenu.Append("</span></a>");
+                        stringMenu.Append("</li>");
+                    }
+                    stringMenu.Append("</ul>");
+                }
+
+
+                stringMenu.Append("</li>");
+            }
+
+            stringMenu.Append("</ul>");
+            return stringMenu.ToString();
+        }
+    }
+}
