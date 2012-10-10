e06234b48044b5cff531ca061bd170da0a132155
nacho692@gmail.com <nacho692@gmail.com@b374577a-d9eb-3a0c-b418-3904471a3b07>
2012-10-10 00:26:20 +0000

    Cambios en permisos y servicios y clases nuevas

---
+++ Depositos del Oeste/trunk/BackEnd/Grupos/Grupo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+
+namespace BackEnd
+{
+    public class Grupo:BusinessObject<Grupo>
+    {
+        private int id;
+        public virtual int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string descripcion;
+        public virtual string Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+    }
+}
---
+++ Depositos del Oeste/trunk/Services/ServicePermissions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BackEnd;
+
+namespace Services
+{
+    public class ServicePermisos
+    {
+        public static bool VerificarPermisos(Usuario user, int menu)
+        {
+            Permiso permiso = new Permiso();
+            permiso.IdGrupo = user.IdGrupo;
+            permiso.IdMenu = menu;
+
+            if (permiso.Select().Count == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static List<Permiso> CargarPermisos(Usuario user)
+        {
+            Permiso permiso = new Permiso();
+            permiso.IdGrupo = user.IdGrupo;
+            return permiso.Select();
+        }
+    }
+}
