16a30133ef823bb2b59143cf35e3033aa8cf6998
nacho692@gmail.com <nacho692@gmail.com@b374577a-d9eb-3a0c-b418-3904471a3b07>
2012-10-10 00:26:20 +0000

    Cambios en permisos y servicios y clases nuevas

---
+++ Depositos del Oeste/trunk/BackEnd/Permisos/Permiso.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+
+namespace BackEnd
+{
+    public class Permiso : BusinessObject<Permiso>
+    {
+        private int id;
+        public virtual int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private int idGrupo;
+        public virtual int IdGrupo
+        {
+            get { return idGrupo; }
+            set { idGrupo = value; }
+        }
+
+        private int idMenu;
+        public virtual int IdMenu
+        {
+            get { return idMenu; }
+            set { idMenu = value; }
+        }
+    }
+}
--- Depositos del Oeste/trunk/Services/ServiceMenu.cs
+++ Depositos del Oeste/trunk/Services/ServiceMenu.cs
@@ -10,7 +10,18 @@ namespace Services
     public class ServiceMenu
     {
         //TODO: Implementar el tema de los privilegios para el usuario
+        
         public static string generarMenu(Usuario user){
+            string inicio = "<ul><li class='has-sub'><a href='#'><span>Inicio</span></a></li></ul>";
+
+            if (user == null)
+            {
+                throw new Exception("Usuario es nulo");
+            }
+            if (user.Legajo == -1)
+            {
+                return inicio;
+            }
             //Traigo todos los menues
             BackEnd.Menu menu = new BackEnd.Menu();
             List<BackEnd.Menu> menuList = menu.Select();
@@ -26,6 +37,12 @@ namespace Services
                     return mn.IdPadre == 0;
                 }
             );
+            for (int i = 0; menuPrincipal.Count > i; i++)
+            {
+
+            }
+            //Cargo los permisos del usuario
+            List<Permiso> permisos = ServicePermisos.CargarPermisos(user);
 
             //Empieza aca, se puede hacer recursivo pero son solo dos niveles estaticos, no hace falta
             stringMenu.Append("<ul>");
@@ -33,7 +50,6 @@ namespace Services
             {
                 //Agarro todos los que son hijos del padre que voy iterando
                 List<BackEnd.Menu> menuSecundario = new List<BackEnd.Menu>();
-                menuSecundario = new List<BackEnd.Menu>();
                 menuSecundario = menuList.FindAll(
                     delegate(BackEnd.Menu mn)
                     {
@@ -48,17 +64,25 @@ namespace Services
                 stringMenu.Append(menuPrincipal[i].Nombre);
                 stringMenu.Append("</span></a>");
 
+                
                 if (menuSecundario.Count > 0)
                 {
                     stringMenu.Append("<ul>");
                     for (int j = 0; menuSecundario.Count > j; j++)
                     {
+
+                    if(permisos.Find(
+                        delegate(Permiso pr){
+                            return pr.IdMenu == menuSecundario[j].Id;
+                    }) != null)
+                        {
                         stringMenu.Append("<li class='has-sub'><a href='");
                         stringMenu.Append(menuSecundario[j].Link);
                         stringMenu.Append("'><span>");
                         stringMenu.Append(menuSecundario[j].Nombre);
                         stringMenu.Append("</span></a>");
                         stringMenu.Append("</li>");
+                        }
                     }
                     stringMenu.Append("</ul>");
                 }
