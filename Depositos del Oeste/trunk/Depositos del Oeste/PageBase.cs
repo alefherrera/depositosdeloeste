be968787f462d904e3aca0de7bdb2fa18f3df1f5
nacho692@gmail.com <nacho692@gmail.com@b374577a-d9eb-3a0c-b418-3904471a3b07>
2012-10-10 02:24:07 +0000

    Deberia de estar funcionando la verificacion de permisos cuando entra a pagina indebida. Menu funcionando y logueo, hay que empezar a laburar nomas

--- Depositos del Oeste/trunk/Depositos del Oeste/PageBase.cs
+++ Depositos del Oeste/trunk/Depositos del Oeste/PageBase.cs
@@ -1,5 +1,6 @@
-﻿
-using BackEnd;
+﻿using BackEnd;
+using System.Web;
+
 namespace Depositos_del_Oeste
 {
     public class PageBase : System.Web.UI.Page
@@ -17,7 +18,16 @@ namespace Depositos_del_Oeste
             base.OnPreLoad(e);
         
             //esta hardcodeado porque me pedia un numero, vos hacete la funcionciata de verificar ;)
-            Services.ServicePermisos.VerificarPermisos(user,"");
+            
+            string context = Request.AppRelativeCurrentExecutionFilePath.Replace("~", "").ToLower();
+            if (context == "/default.aspx")
+            {
+                return;
+            }
+            if (!Services.ServicePermisos.VerificarPermisos(user, context))
+            {
+                Response.Redirect("/Default.aspx");
+            }
         }
     }
 }
