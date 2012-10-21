c040e2582705d84a0e6716fd2a076a9b70a5c3a0
nacho692@gmail.com <nacho692@gmail.com@b374577a-d9eb-3a0c-b418-3904471a3b07>
2012-10-21 14:42:59 +0000

    Cambios en el trasfondo de select(string query) para que devuelva una hashtable. Despues tenes que moldearla como la necesites. En vez de ser [columna][row] es [row][columna] pero mas incomodo de usar.
    Consulta de ubicaciones terminado.

---
+++ Depositos del Oeste/trunk/BackEnd/Utils/Enums.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd.Utils
+{
+    public class Enums
+    {
+        public enum Ubicaciones_Actividad
+        {
+            Baja =  0,
+            Media = 1, 
+            Alta = 2
+        }
+        public static string ActividadDesc(int actividad)
+        {
+            Enums.Ubicaciones_Actividad enumDisplay = ((Enums.Ubicaciones_Actividad)actividad);
+            return enumDisplay.ToString();
+        }
+
+        public enum Ubicaciones_Estado
+        {
+            Libre = 0,
+            Reservada = 1,
+            Ocupada = 2
+        }
+        public static string EstadoDesc(int estado)
+        {
+            Enums.Ubicaciones_Estado enumDisplay = ((Enums.Ubicaciones_Estado)estado);
+            return enumDisplay.ToString();
+        }
+    }
+}
---
+++ Depositos del Oeste/trunk/Depositos del Oeste/ubicaciones/ubicaciones.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BackEnd;
+using Services;
+
+namespace Depositos_del_Oeste
+{
+    public partial class _Ubicaciones : PageBase
+    {
+         protected void Page_Load(object sender, EventArgs e)
+          {
+            if (!IsPostBack)
+                ServiceUbicaciones.cargarComboEstanteria(ddlEstanteria);
+            ServiceUbicaciones.cargarGridEstanteria(gridUbicaciones, ddlEstanteria.SelectedItem.Value);
+        }
+
+        protected void ddlEstanteria_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
