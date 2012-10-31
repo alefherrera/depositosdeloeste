b94a2fc751e21b866f58e1f8a4949aa4a8eb5bde
alef.herrera@gmail.com <alef.herrera@gmail.com@b374577a-d9eb-3a0c-b418-3904471a3b07>
2012-10-31 02:29:41 +0000

    reservas detalle

---
+++ Depositos del Oeste/trunk/BackEnd/ReservasDetalle/ReservaDetalle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd.ReservasDetalle
+{
+    public class ReservaDetalle : BusinessObject<ReservaDetalle>
+    {
+        public virtual int IdCompartimiento{ get; set; }
+        public virtual string CodigoReserva { get; set; }
+        public virtual int IdArticulo { get; set; }
+        public virtual int Cantidad{ get; set; }
+    }
+}
