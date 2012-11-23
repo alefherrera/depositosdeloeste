7f28e012b8c9caa640a84c0b019ee6124dca7ada
nacho692@gmail.com <nacho692@gmail.com@b374577a-d9eb-3a0c-b418-3904471a3b07>
2012-11-23 00:07:03 +0000

    Arregle productos_baja, no estaba funcionando. Arregle productos_alta, algunas validaciones. Agregue una mejora en ingresos, para que primero ingrese en los que tienen cosas ocupadas.

--- Depositos del Oeste/trunk/BackEnd/ReservasDetalle/ReservaDetalle.cs
+++ Depositos del Oeste/trunk/BackEnd/ReservasDetalle/ReservaDetalle.cs
@@ -13,6 +13,32 @@ namespace BackEnd
         public virtual int IdArticulo { get; set; }
         public virtual int Cantidad{ get; set; }
 
+        public virtual Compartimiento Compartimiento { get; set; }
+
+        public override bool Load()
+        {
+            base.Load();
+            Compartimiento cmp = new Compartimiento();
+            cmp.Id = this.IdCompartimiento;
+            cmp.Load();
+            Compartimiento = cmp;
+            if (this.Loaded && cmp.Loaded)
+                return true;
+            return false;
+        }
+
+        public override List<ReservaDetalle> Select()
+        {
+            List<ReservaDetalle> lista = base.Select();
+            foreach (ReservaDetalle detalle in lista)
+            {
+                detalle.Compartimiento = new Compartimiento();
+                detalle.Compartimiento.Id = detalle.IdCompartimiento;
+                detalle.Compartimiento.Load();
+            }
+            return lista;
+        }
+
         public override bool Equals(object obj)
         {
             ReservaDetalle reservaD = obj as ReservaDetalle;
