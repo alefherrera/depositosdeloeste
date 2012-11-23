86b07ba3b7a417ac3cd5ead830cd8415ea25226d
alef.herrera@gmail.com <alef.herrera@gmail.com@b374577a-d9eb-3a0c-b418-3904471a3b07>
2012-11-23 23:27:05 +0000

    solucionado el error de reportes

--- Depositos del Oeste/trunk/Depositos del Oeste/reportes/reportes.aspx.cs
+++ Depositos del Oeste/trunk/Depositos del Oeste/reportes/reportes.aspx.cs
@@ -13,6 +13,7 @@ namespace Depositos_del_Oeste
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            lbError.Text = "";
             if (!IsPostBack)
             {
                 CargarComboMes();
@@ -48,16 +49,23 @@ namespace Depositos_del_Oeste
             int total = 0;
             System.Data.DataRow dr;
             System.Data.DataTable dt = BusinessObject<object>.SelectSQL("Call reportes (" + ddlMes.SelectedValue + "," + ddlAno.SelectedValue + ")");
-            foreach (System.Data.DataRow row in dt.Rows)
+            if (dt.Rows.Count > 0)
             {
-                total += Convert.ToInt32(row["porcentaje"].ToString().Replace("%",""));
-                row["fecha"] = row["fecha"].ToString().Split(' ')[0];
+                foreach (System.Data.DataRow row in dt.Rows)
+                {
+                    total += Convert.ToInt32(row["porcentaje"].ToString().Replace("%", ""));
+                    row["fecha"] = row["fecha"].ToString().Split(' ')[0];
+                }
+                dr = dt.NewRow();
+                dr["fecha"] = "Total";
+                dr["porcentaje"] = (total / dt.Rows.Count).ToString() + "%";
+                dt.Rows.Add(dr);
+                tblReporte.DataSource = dt;
             }
-            dr = dt.NewRow();
-            dr["fecha"] = "Total";
-            dr["porcentaje"] = (total / dt.Rows.Count).ToString() + "%";
-            dt.Rows.Add(dr);
-            tblReporte.DataSource = dt;
+            else
+            {
+                lbError.Text = "No se encontraron reportes en el mes seleccionado";
+            }
             tblReporte.DataBind();
         }
 
--- Depositos del Oeste/trunk/Depositos del Oeste/reportes/reportes.aspx.designer.cs
+++ Depositos del Oeste/trunk/Depositos del Oeste/reportes/reportes.aspx.designer.cs
@@ -65,5 +65,14 @@ namespace Depositos_del_Oeste {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView tblReporte;
+        
+        /// <summary>
+        /// lbError control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbError;
     }
 }
